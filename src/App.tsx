cce11c62a10460933bf01ea8fbe28df376835db4
PopovDS617 <popovds.bm@gmail.com>
2023-04-11 14:51:11 +0300

    feat: add app component, assemble all parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useAsync } from './hooks/useAsync';
+import { ApiResponse } from './models/api-response';
+import { getTime } from './utils/getTime';
+import { Clock } from './components/Clock';
+
 export const App = () => {
-  return <div className="App">hello</div>;
+  const { value, error } = useAsync<ApiResponse>(getTime, []);
+
+  return (
+    <div className="app">
+      {error && <p>Error</p>}
+
+      {!error && <Clock data={value} />}
+    </div>
+  );
 };
